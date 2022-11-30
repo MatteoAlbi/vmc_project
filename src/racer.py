d091742208641b2e38feb61c351e3c53af519ad8
MatteoAlbi <matteo.albi99@gmail.com>
2022-11-30 17:38:22 +0200

    commenting code + best parameter tuning

--- src/racer.py
+++ src/racer.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 
-from re import S
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist
@@ -16,44 +15,65 @@ class PotentialField:
     def __init__(self, need_plot=False):
 
         self.lidar_readings = []
-        self.x = 0
-        self.y = 0
-        self.lidar_threshold = 2
+        #self.x = 0
+        #self.y = 0
+        # threshold for the lidar: further points are not considered
+        self.lidar_threshold = 2 
+        # cap for the potential: higher values are capped to this value
+        self.pot_cap = 15000
+
+        # field dimensions
         self.field_x = 2
         self.field_y = 1
+        # field position offset wrt robot (offset = 0 -> robot in bottom left corner)
         self.field_offset_x = 0
         self.field_offset_y = 0.5
+        # number of points per axis
         self.field_grid_x = 20
         self.field_grid_y = 20
-        self.pot_gain = 1
+        # create grid for the potential
+        self.pot_x_points = np.linspace(0-self.field_offset_x, self.field_x-self.field_offset_x, self.field_grid_x)
+        self.pot_y_points = np.linspace(0-self.field_offset_y, self.field_y-self.field_offset_y, self.field_grid_y)
+
+        # gain for the potential: pot = gain/dist(x,y)
+        self.pot_gain = 1 # gain for repulsive points
+        self.attr_gain = 1000 # gain for attraction point
+        
+        # store computed trajectory
         self.traj_idx = []
-        self.pot_cap = 15000
+        # bool variable to display potential 
         self.need_plot = need_plot
+
+        # initialization
         self.pot_field = np.zeros((self.field_grid_x,self.field_grid_y))
         self.attr_point = [self.field_x, 0]
-        self.attr_gain = 1000
-
+        
 
         if self.need_plot:
+            # plot window
             _, self.ax = plt.subplots(1,2, figsize=(12,6))
             self.ax[1] = sb.heatmap(self.pot_field, cbar=False)
             self.ax[1].invert_yaxis()
 
 
     def plot_vision(self):
+        # unpack x and y coordinates for each point
         x, y = zip(*self.cartesian)
         self.ax[0].clear()
+        # cartesian coordinates of the lidar readings
         self.ax[0].scatter([ -i for i in list(y)],x)
         self.ax[0].scatter(0,0)
         self.ax[0].set_xlim([-self.field_offset_y, self.field_y-self.field_offset_y])
         self.ax[0].set_ylim([-self.lidar_threshold, self.lidar_threshold])
         self.ax[0].grid()
+        # heatmap of the potential
         y_tick = np.linspace(0-self.field_offset_x, self.field_x-self.field_offset_x, self.field_grid_x)
         x_tick = np.linspace(0-self.field_offset_y, self.field_y-self.field_offset_y, self.field_grid_y)
-        y_tick = np.around(y_tick,decimals=2)[::-1]
-        x_tick = np.around(x_tick,decimals=2)[::-1]
-        #We flip on the first axis since seaborn would plot it tother way.
-        #We flip on the second axis since the y direction is positive to the left, in the opposite way as the arraaaay would go.
+        
+        x_tick = np.around(x_tick,decimals=2)[::-1] # We flip on the first axis since seaborn would plot it the other way
+        y_tick = np.around(y_tick,decimals=2)[::-1] # We flip on the second axis since the y direction is positive to the left, in the opposite way as the array would go
+        
+        #plot
         self.ax[1] = sb.heatmap(self.pot_field[::-1,::-1], xticklabels=x_tick, yticklabels=y_tick, cbar=False)#, ax=self.ax[1])
         plt.pause(0.0001)
         
@@ -68,55 +88,56 @@ class PotentialField:
                 self.cartesian.append([np.cos(i*np.pi/180)*d, np.sin(i*np.pi/180)*d])
 
         # Potential Field
-        self.pot_x_points = np.linspace(0-self.field_offset_x, self.field_x-self.field_offset_x, self.field_grid_x)
-        self.pot_y_points = np.linspace(0-self.field_offset_y, self.field_y-self.field_offset_y, self.field_grid_y)
-        for i,x in enumerate(self.pot_x_points):
-            for j,y in enumerate(self.pot_y_points):
-                for p in self.cartesian:
+        
+        for i,x in enumerate(self.pot_x_points): # x coordinate on potential grid
+            for j,y in enumerate(self.pot_y_points): # y coordinate of potential grid
+                for p in self.cartesian: # lidar points
+                    # distance function
                     dist_sqr = (x-p[0])**2 + abs(y-p[1])#**2
+                    # potential contribute of the point
                     self.pot_field[i,j] += self.pot_gain / (dist_sqr)
+                
+                # attractive point potential
                 dist_sqr = abs(x-self.attr_point[0]) + (y-self.attr_point[1])**2
                 self.pot_field[i,j] -= self.attr_gain / np.sqrt(dist_sqr)
+                
+                # apply potential cap
                 self.pot_field[i,j] = min(self.pot_field[i,j], self.pot_cap)
                 self.pot_field[i,j] = max(self.pot_field[i,j], -self.pot_cap)
     
     
-    def make_trajectory(self, prev_traj):
-        # trajectory
+    def make_trajectory(self):
         finish = False
-        #offset = [(1,0),(0,1),(1,1),(1,-1),(0,-1)]
+        # offset in grid coordinates
         offset = [(1,0),(1,1),(1,-1)]
         self.traj_idx = []
-        curr_point = [0,int(self.field_grid_y/2)]
+        # current position of the robot in the potential grid
+        curr_point = [int( self.field_grid_x * self.field_offset_x / self.field_x), int( self.field_grid_y * self.field_offset_y / self.field_y)]
         self.traj_idx.append(curr_point)
         
-        point_count = 0
+        point_count = 0 # limit number of point of trajectory as function of number of point in x direction
         while not finish and point_count < int(self.field_grid_x/1.5):
-
-            #i_min = np.argmin([ self.pot_field[curr_point[0] + offset[0][0], curr_point[1] + offset[0][1]],\
-            #                    self.pot_field[curr_point[0] + offset[1][0], curr_point[1] + offset[1][1]],\
-            #                    self.pot_field[curr_point[0] + offset[2][0], curr_point[1] + offset[2][1]],\
-            #                    self.pot_field[curr_point[0] + offset[3][0], curr_point[1] + offset[3][1]],\
-            #                    self.pot_field[curr_point[0] + offset[4][0], curr_point[1] + offset[4][1]] ])
-            
+            # check potential in front points, choose point with lowest potential            
             i_min = np.argmin([ self.pot_field[curr_point[0] + offset[0][0], curr_point[1] + offset[0][1]],\
                                 self.pot_field[curr_point[0] + offset[1][0], curr_point[1] + offset[1][1]],\
                                 self.pot_field[curr_point[0] + offset[2][0], curr_point[1] + offset[2][1]] ])
-
-            self.pot_field[curr_point[0],curr_point[1]] += self.pot_cap
+            # increment curr point before switching to visualize trajectory
+            if self.need_plot: self.pot_field[curr_point[0],curr_point[1]] += self.pot_cap
+            # switch point and add to traj
             curr_point = [curr_point[0] + offset[i_min][0], curr_point[1] + offset[i_min][1]]
             self.traj_idx.append(curr_point)
 
+            # termination condition: extremes of the grid
             if curr_point[0] == self.field_grid_x-1 or curr_point[1] == 0 or curr_point[1] == self.field_grid_y-1:
                 finish = True
-            point_count+=1
+            point_count += 1
 
+        # switch from grid coordinates to local coordinates
         trajectory = np.array([[self.pot_x_points[x[0]], self.pot_y_points[x[1]]] for x in self.traj_idx])
         trajectory = np.around(trajectory, decimals=2)
+        # set attraction point as last point of the trajectory
         self.attr_point = trajectory[-1]
         
-
-        #print("Computed trajectory:\n",trajectory,"\n")
         if self.need_plot:
             self.plot_vision()    
 
@@ -127,6 +148,7 @@ class PotentialField:
 class TurtleBot:
 
     def call_position(self, msg):
+        # get x,y,yaw of robot from subscriber
         euler_angles = euler_from_quaternion(  [msg.pose.pose.orientation.x,
                                                 msg.pose.pose.orientation.y,
                                                 msg.pose.pose.orientation.z,
@@ -136,23 +158,8 @@ class TurtleBot:
         self.yaw = euler_angles[2]
 
     def call_Lidar(self, msg):
+        # get lidar readings from subscriber
         self.lidar_readings = msg.ranges
-        self.front_reading = msg.ranges[0]
-        self.left_reading = msg.ranges[90]
-        self.right_reading = msg.ranges[270]
-
-    def avoid_walls(self):
-        left_space = np.min([np.abs(self.left_reading - self.y),1])
-        right_space = np.min([np.abs(self.right_reading - self.y),1])
-        self.error =  np.max([np.min([left_space - right_space,1]),-1])
-        self.cumulative_error += self.error * self.sample_time
-
-        self.vel.angular.z =  self.Kp * self.error + \
-                              self.Kd * (self.error - self.prev_error)/self.sample_time +\
-                              self.Ki * (self.cumulative_error)
-
-        self.prev_error = self.error
-        #self.pub.publish(self.vel)
 
     def pure_pursuit(self,traj):
         xt = list(x[0] for x in traj)
@@ -192,6 +199,8 @@ class TurtleBot:
 
     def exit_control(self):
         self.out = True
+        # control set of frontal points to asses if the track is complete
+
         for i in range(45, 90, 1):
             if self.lidar_readings[i] != float("inf"):
                 self.out = False
@@ -200,7 +209,7 @@ class TurtleBot:
             if self.lidar_readings[i] != float("inf"):
                 self.out = False
             
-
+        # stop robot
         if self.out:
             self.vel.linear.x = 0
             self.vel.angular.z = 0
@@ -208,59 +217,67 @@ class TurtleBot:
             print(".............Can't find left/right wall, exiting...........")
         
     def __init__(self):
+        
         self.sample_time = 0.1
-        self.out = False
-        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1/self.sample_time)
-        self.vel = Twist()
-        self.vel.linear.x = 0.4
-        self.vel.angular.x = 0
-        self.vel.angular.y = 0
-        self.vel.angular.z = 0
-        self.pub.publish(self.vel)
         self.rate = rospy.Rate(1/self.sample_time) 
 
+        # robot pos+attitude and lidar
         self.x = 0
         self.y = 0
         self.yaw = 0
 
         self.lidar_readings = []
-        self.front_reading = 0
-        self.left_reading = 0
-        self.right_reading = 0
 
-        self.Kp = 0.5
-        self.Kd = 0.05
-        self.Ki = 0.02
+        # PID parameters
+        self.Kp = 0.55
+        self.Kd = 0.04
+        self.Ki = 0.01
         self.error = 0
         self.prev_error = 0
         self.cumulative_error = 0
-        self.max_v = 1.4
 
+        # pure pursuit parameters
+        self.max_v = 1.3
         self.look_ahead = 0.5
         self.traj = []
-        self.k_brake_large = 0.5
+        self.k_brake_large = 0.4
         self.k_brake_ahead = 0.8
 
-        rospy.Subscriber('odom', Odometry, self.call_position)
+        # subscriber to retrieve x,y,yaw and lidar readings
+        #rospy.Subscriber('odom', Odometry, self.call_position) # not used
         rospy.Subscriber('scan', LaserScan, self.call_Lidar)
 
+        # initialize potential field object to compute trajectory
         PotField = PotentialField(need_plot=False)
 
-        while not rospy.is_shutdown() and not self.out:
-            
+        # control velocitty message
+        self.vel = Twist()
+        self.vel.linear.x = self.max_v 
+        self.vel.angular.x = 0
+        self.vel.angular.y = 0
+        self.vel.angular.z = 0
+
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1/self.sample_time)
+        self.pub.publish(self.vel)
+
+        self.out = False # stop boolean
+        
+        # running loop
+        while not rospy.is_shutdown() and not self.out: 
+            # compute trajectory using potential field
             PotField.lidar_readings = self.lidar_readings
             PotField.create_field()
-            self.traj = PotField.make_trajectory(self.traj)
+            self.traj = PotField.make_trajectory()
+            # pure pursuit trajectory follower
             self.pure_pursuit(self.traj)
+            # check stop condition
             if len(self.lidar_readings) > 0:
                 self.exit_control()
-
-            #print("Commanded velocity: "+str(self.vel.angular.z))
-            #print("Yaw: "+str(self.yaw)
             
             self.rate.sleep()
 
 if __name__ == '__main__':
+    # run node
     rospy.init_node('driver')
     try:
         Tbot = TurtleBot()          
